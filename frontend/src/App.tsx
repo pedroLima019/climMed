fb20b754fd5d8b6fa631c556fd28e4eacf7b30ac
pedroLima019 <pedroliveiralima21@gmail.com>
2025-08-22 17:39:30 -0300

    feat(login): tela de login para paciente e medico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import "./styles/global.css";
-import Welcome from "./pages/welcome";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <Welcome />;
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login/medico" element={<Login role="MEDICO" />} />
+        <Route path="/login/paciente" element={<Login role="PACIENTE" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -1,7 +1,17 @@
 import "../styles/global.css";
 import "../styles/welcome.css";
+import { useNavigate } from "react-router-dom";
 
 export function Welcome() {
+  const navigation = useNavigate();
+
+  const handleSelectRole = (role: "MEDICO" | "PACIENT") => {
+    if (role === "MEDICO") {
+      navigation("/login/medico");
+    } else {
+      navigation("/login/paciente");
+    }
+  };
   return (
     <main>
       <div className="welcome-container">
@@ -13,11 +23,21 @@ export function Welcome() {
           <p>Você se indentifica como ?</p>
           <form className="form-container">
             <div className="user">
-              <input type="radio" id="medico" name="tipoUsuario" />
+              <input
+                type="radio"
+                id="medico"
+                name="tipoUsuario"
+                onClick={() => handleSelectRole("MEDICO")}
+              />
               <label htmlFor="medico">Médico</label>
             </div>
             <div className="user">
-              <input type="radio" id="paciente" name="tipoUsuario" />
+              <input
+                type="radio"
+                id="paciente"
+                name="tipoUsuario"
+                onClick={() => handleSelectRole("PACIENT")}
+              />
               <label htmlFor="paciente">Paciente</label>
             </div>
           </form>
