1c669b94c08d82691aec11f1c946301000da0e19
Pedro Lima <135082090+pedroLima019@users.noreply.github.com>
2025-08-21 21:15:07 -0300

    Delete climMed directory

--- climMed/backend/src/middleware/isDoctor.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.user || req.user.role !== "DOCTOR") {
-    return res
-      .status(403)
-      .json({ error: "Apenas médicos têm permissão para acessar está rota" });
-  }
-  next();
-};
